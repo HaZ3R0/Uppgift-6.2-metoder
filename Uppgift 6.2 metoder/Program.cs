abec4e2ff39ad729d12c80dabcb55e032a837820
HaZ3R0 <megamackan@outlook.com>
2024-02-14 10:40:36 +0100

    Add project files.

---
+++ Uppgift 6.2 metoder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace hej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Största talet var " + Störst(tal1, tal2));
+        }
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 >= tal2)
+            {
+                return tal1;
+            }
+            else
+            {
+                return tal2;
+            }
+        }
+    }
+}
